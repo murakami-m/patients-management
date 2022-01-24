82a203a0376f82494e4cb0a7a579b46aa913e8d8
murakami-m <senazuki0825@yahoo.co.jp>
2022-01-24 18:24:16 +0900

    患者情報編集機能の実装

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -15,6 +15,20 @@ class PatientsController < ApplicationController
     @patient = Patient.find(params[:id])
   end
 
+  def edit
+    @patient = Patient.find(params[:id])
+  end
+
+  def update
+    @patient = Patient.find(params[:id])
+    @patient.update(patient_params)
+    if @patient.update(patient_params)
+      redirect_to patient_path
+    else
+      render :edit
+    end
+  end
+
   private
   
   def patient_params
