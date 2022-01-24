cb9e7f5dab4d63b6e2175f6b6a1fd70362a2b68a
murakami-m <senazuki0825@yahoo.co.jp>
2022-01-24 11:43:25 +0900

    Doctor及びmanagerのデータ修正

--- app/models/adjustment_manager.rb
+++ app/models/adjustment_manager.rb
@@ -1,9 +1,9 @@
 class AdjustmentManager < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
-    { id: 2, name: '島田' },
-    { id: 3, name: '水野' },
-    { id: 4, name: '桜井' }
+    { id: 2, name: '水野' },
+    { id: 3, name: '桜井' },
+    { id: 4, name: '高野' }
   ]
 
   include ActiveHash::Associations
--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,11 +1,11 @@
 class Doctor < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
-    { id: 2, name: '院長' },
-    { id: 3, name: '菊地Dr' },
-    { id: 4, name: '杉本Dr' },
-    { id: 5, name: '古川Dr' },
-    { id: 6, name: '大西Dr' }
+    { id: 2, name: '菊地Dr' },
+    { id: 3, name: '杉本Dr' },
+    { id: 4, name: '古川Dr' },
+    { id: 5, name: '大西Dr' },
+    { id: 6, name: '島田' }
   ]
 
   include ActiveHash::Associations
